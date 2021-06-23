39ddcf1d51c1f82220a50e31e1d3025c641c7d5e
Manh vu <vuvanmanhttvtvp2@gmail.com>
2021-06-23 19:19:33 +0700

    AS06

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace AS6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.startButton = new System.Windows.Forms.Button();
+            this.stopBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(-3, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 334);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(81, 375);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(127, 31);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start the tour";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(598, 375);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(127, 31);
+            this.stopBtn.TabIndex = 2;
+            this.stopBtn.Text = "Stop the tour";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button stopBtn;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace AS6
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine listener = new SpeechRecognitionEngine();
+        SpeechSynthesizer Tam = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listener.RecognizeAsync(RecognizeMode.Multiple);
+            startButton.Enabled = false;
+            stopBtn.Enabled = true;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listener.RecognizeAsyncCancel();
+            startButton.Enabled = true;
+            stopBtn.Enabled = false;
+            pictureBox1.Image = Image.FromFile(@"..\..\Pictures\Choice.png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listener.SetInputToDefaultAudioDevice();
+            listener.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices("Hello", "Garden", "Kitchen"))));
+            listener.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Speech_recognized);
+            stopBtn.Enabled = false;
+        }
+
+        private void Speech_recognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string s = e.Result.Text;
+            switch (s)
+            {
+                case "Hello":
+                    pictureBox1.Image = Image.FromFile(@"..\..\Pictures\Welcome.png");
+                    break;
+                case "Garden":
+                    pictureBox1.Image = Image.FromFile(@"..\..\Pictures\Garden.png");
+                    break;
+                case "Kitchen":
+                    pictureBox1.Image = Image.FromFile(@"..\..\Pictures\Kitchen.png");
+                    break;
+                default:
+                    pictureBox1.Image = Image.FromFile(@"..\..\Pictures\Sorry.png");
+                    break;
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
